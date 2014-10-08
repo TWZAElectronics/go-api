49f5fa4983775799afe8b15b4c7021272df904cf
ldanster <lebohangdanster@gmail.com>
2014-10-08 15:23:43 +0200

    remove all the multiple acks for each command

--- src/app.py
+++ src/app.py
@@ -10,53 +10,47 @@ class PipelineApp():
 
     def process_command(self, command):
 
+        self.board.write("ACK")
+
         if command.startswith("CMD_READY"):
             print("key pressed")
-            self.board.write("ACK")
             output = self.controller.current()
             print(output)
             self.reply(output)
 
         elif command.startswith("CMD_MODE"):
             print("switch between PIPELINE and STAGE modes")
-            self.board.write("ACK")
             output = self.controller.switch_mode()
             print(output)
             self.reply(output)
 
         elif command.startswith("CMD_DEPLOY"):
             print("redeploy a selected STAGE or PIPELINE")
-            self.board.write("ACK")
             output = self.controller.deploy()
             print(output)
             self.reply(output)
 
         elif command.startswith("CMD_DEPLOY_TO"):
             print("deploy to the NEXT STAGE in a pipeline")
-            self.board.write("ACK")
 
         elif command.startswith("CMD_BACK"):
             print("scroll back through a stage or pipeline depending on current mode")
-            self.board.write("ACK")
             output = self.controller.previous()
             print(output)
             self.reply(output)
 
         elif command.startswith("CMD_FORWARD"):
             print("scroll forward through a stage or pipeline depending on current mode")
-            self.board.write("ACK")
             output = self.controller.next()
             print(output)
             self.reply(output)
 
         elif command.startswith("CMD_EXIT"):
             print("Exiting application")
-            self.board.write("ACK")
             return False
 
         else:
             print("unknown command, please try again")
-            self.board.write("ACK")
         return True
 
     def read_board(self):
