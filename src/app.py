03a05a6c66511d1277663685cef93862fe28f6e1
Charles Boadu <cboadu@thoughtworks.com>
2014-10-08 15:14:35 +0200

    Returns ACK to client

--- src/app.py
+++ src/app.py
@@ -12,51 +12,61 @@ class PipelineApp():
 
         if command == "CMD_READY":
             print("key pressed")
+            self.board.write("ACK")
             output = self.controller.current()
             print(output)
             self.reply(output)
 
         elif command == "CMD_MODE":
             print("switch between PIPELINE and STAGE modes")
+            self.board.write("ACK")
             output = self.controller.switch_mode()
             print(output)
             self.reply(output)
 
         elif command == "CMD_DEPLOY":
             print("redeploy a selected STAGE or PIPELINE")
+            self.board.write("ACK")
             output = self.controller.deploy()
             print(output)
             self.reply(output)
 
         elif command == "CMD_DEPLOY_TO":
             print("deploy to the NEXT STAGE in a pipeline")
+            self.board.write("ACK")
 
         elif command == "CMD_BACK":
             print("scroll back through a stage or pipeline depending on current mode")
+            self.board.write("ACK")
             output = self.controller.previous()
             print(output)
             self.reply(output)
 
         elif command == "CMD_FORWARD":
             print("scroll forward through a stage or pipeline depending on current mode")
+            self.board.write("ACK")
             output = self.controller.next()
             print(output)
             self.reply(output)
 
         elif command == "exit":
             print("Exiting application")
+            self.board.write("ACK")
             return False
 
         else:
             print("unknown command, please try again")
+            self.board.write("ACK")
         return True
 
     def read_board(self):
         return self.board.read()
 
     def reply(self, message):
-        self.board.write("ACK")
-        self.board.write(message)
+        counter = 0
+        while counter < 10 and self.read_board() != "ACK":
+            counter+=1
+            self.board.write("DSP:LN2:SCR0:CMXA:" + message)
 
 
 if __name__ == "__main__":
@@ -64,6 +74,7 @@ if __name__ == "__main__":
     running = True
     while running:
         command = app.read_board()
+        print command
         running = app.process_command(command)
 
 
